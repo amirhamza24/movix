a69933cb5d2cdb5aa9c7b8647a6f15e6a9fae996
amirhamza24 <ridoykhan7897890@gmail.com>
2023-11-07 15:50:04 +0600

    hooks setup

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 import "./style.scss"
 
 const HeroBanner = () => {
+
+    const [background, setBackground] = useState("");
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+
+    const searchQueryHandler = (event) => {
+        if(event.key === "Enter" && query.length > 0) {
+            navigate(`/search/${query}`)
+        }
+    }
+
   return (
     <div className="heroBanner">
         <div className="wrapper">
@@ -12,6 +24,15 @@ const HeroBanner = () => {
                     Millions of Movies, TV shows and people to discover.
                     Explore now.
                 </span>
+
+                <div className="searchInput">
+                    <input 
+                        type="text" 
+                        placeholder="Search for a movies or tv show..."
+                        onChange={(e) => setQuery(e.target.value)} 
+                        onKeyUp={searchQueryHandler} />
+                    <button>Search</button>
+                </div>
             </div>
         </div>
     </div>
