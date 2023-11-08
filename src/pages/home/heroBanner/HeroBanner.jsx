fe35739ef2c8e80da8a97a3f39bbefde3da07a78
amirhamza24 <ridoykhan7897890@gmail.com>
2023-11-08 19:59:10 +0600

    bg img set from api

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,13 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import "./style.scss"
+import useFetch from '../../../hooks/useFetch';
+
+import Img from "../../../components/lazyLoadImage/Img";
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 
 const HeroBanner = () => {
 
     const [background, setBackground] = useState("");
     const [query, setQuery] = useState("");
     const navigate = useNavigate();
+    const {url} = useSelector((state) => state.home);
+    const { data, loading } = useFetch("/movie/upcoming");
+
+    useEffect( () => {
+        const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+        setBackground(bg);
+    }, [data])
 
     const searchQueryHandler = (event) => {
         if(event.key === "Enter" && query.length > 0) {
@@ -17,7 +29,20 @@ const HeroBanner = () => {
 
   return (
     <div className="heroBanner">
-        <div className="wrapper">
+
+        {!loading && ( 
+            <div className="backdrop-img">
+                {/* <span className='lazy-load-image-background blur lazy-load-image-loaded' style="color: transparent; display: inline-block;"> */}
+                    <Img src={background} alt="" />
+                {/* </span> */}
+            </div> 
+        )}
+
+        <div className="opacity-layer">
+            
+        </div>
+
+        <ContentWrapper>
             <div className="heroBannerContent">
                 <span className="title">Welcome.</span>
                 <span className="subTitle">
@@ -34,7 +59,7 @@ const HeroBanner = () => {
                     <button>Search</button>
                 </div>
             </div>
-        </div>
+        </ContentWrapper>
     </div>
   )
 }
