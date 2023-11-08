d9aee19cb2815788e0dae9aa4f9e6d336c3a63b9
amirhamza24 <ridoykhan7897890@gmail.com>
2023-11-09 01:31:55 +0600

    header section added with responsive

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import "./style.scss"
 import HeroBanner from './heroBanner/HeroBanner'
 
-HeroBanner
+HeroBanner;
 
 const Home = () => {
   return (
     <div className='homePage'>
         <HeroBanner />
+        <div style={{height: 1000}}></div>
     </div>
   )
 }
