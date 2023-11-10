05cf1d9efc11d26c6d753239926869af71821f66
amirhamza24 <ridoykhan7897890@gmail.com>
2023-11-10 14:45:30 +0600

    search method added

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -6,7 +6,7 @@ import "./style.scss"
 
 import { fetchDataFromApi } from '../../utils/api';
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
-// import MovieCard from "../../components/movieCard/MovieCard";
+import MovieCard from "../../components/movieCard/MovieCard";
 import Spinner from "../../components/spinner/Spinner";
 import noResults from "../../assets/no-results.png";
 
@@ -42,12 +42,46 @@ const SearchResult = () => {
     };
 
     useEffect( () => {
+        setPageNum(1);
         fetchInitialData();
     }, [query])
 
   return (
     <div className='searchResultsPage'>
         {loading && <Spinner initial={true} />}
+
+        {!loading && (
+            <ContentWrapper>
+                {data?.results?.length > 0 ? (
+                    <>
+                        <div className="pageTitle">
+                            {`Search ${data?.total_results > 1 ? "results" : "result"} of '${query}'`}
+                        </div>
+
+                        <InfiniteScroll
+                            className="content"
+                            dataLength={data?.results?.length || []}
+                            next={fetchNextPageData}
+                            hasMore={pageNum <= data?.total_pages}
+                            loader={<Spinner />}
+                        >
+                            {
+                                data?.results.map((item, index) => {
+                                    if(item.media_type === "person") return;
+                                    return (
+                                        <MovieCard key={index} data={item} fromSearch={true} />
+                                    );
+                                })
+                            }
+                        </InfiniteScroll>
+                    </>
+                ) : (
+                    <span className="resultNotFound">
+                        Sorry, Results Not Found!
+                    </span>
+                )}
+            </ContentWrapper>
+        )}
     </div>
   )
 }
