658b9dcf78c245da598d674d9cf85f2ccd3ec6b7
amirhamza24 <ridoykhan7897890@gmail.com>
2023-11-10 15:07:27 +0600

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Home from './pages/home/Home';
 import Details from './pages/details/Details';
 import SearchResult from './pages/searchResult/SearchResult';
 import Explore from './pages/explore/Explore';
-import pageNotFound from "./pages/404/pageNotFound";
+import PageNotFound from "./pages/404/PageNotFound";
 
 
 function App() {
@@ -69,7 +69,7 @@ return (
                 <Route path="/:mediaType/:id" element={<Details />} />
                 <Route path="/search/:query" element={<SearchResult />} />
                 <Route path="/explore/:mediaType" element={<Explore />} />
-                <Route path="*" element={<pageNotFound />} />
+                <Route path="*" element={<PageNotFound />} />
             </Routes>
             <Footer />
         </BrowserRouter>
